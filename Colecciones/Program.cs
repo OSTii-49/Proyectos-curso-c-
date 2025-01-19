e77c0480d213cd354f14185a856ab4457f3389a0
henry <henry49torres@gmail.com>
2025-01-19 18:52:08 -0400

    Colecciones

---
+++ Colecciones/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_WorkingWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car1 = new Car();
+            car1.Make = "Oldsmobile";
+            car1.Modle = "Cutlas Supreme";
+            car1.VIN = "A1";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Modle = "Prism";
+            car2.VIN = "B2";
+
+            Book book1 = new Book();
+            book1.Author = "Robert Tabor";
+            book1.Title = "Microsoft .NET XML Web Services";
+            book1.ISBN = "0=000=00000=0";
+           
+
+          
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(car1);
+            myArrayList.Add(car2);
+            myArrayList.Add(book1);
+            myArrayList.Remove(book1);
+
+            foreach (Car car in myArrayList)
+            {
+                Console.WriteLine(car.Make);
+                
+            }
+            
+            
+
+
+       
+
+            List<Car> myList = new List<Car>();
+            myList.Add(car1);
+            myList.Add(car2);
+        
+
+            foreach (Car car in myList)
+            {
+                Console.WriteLine(car.Make); 
+            }
+            
+
+
+        
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+            myDictionary.Add(car1.VIN, car1);
+            myDictionary.Add(car2.VIN, car2);
+
+            Console.WriteLine(myDictionary["B2"].Make);
+        
+
+
+            string[] names = { "Bob", "Steve", "Brian", "Chuck" };
+
+
+            Car car1 = new Car() { Make = "BMW", Modle = "750li", VIN = "C3" };
+            Car car2 = new Car() { Make = "Toyota", Modle = "4Runner", VIN = "D4" };
+
+        // PARA QUE ESTO FUNCIONE HAY QUE PONER COMO COMENTARIO LA PARTE DE ARRIBA
+            List<Car> myList = new List<Car>
+            {
+                new Car {Make = "Oldsmobile", Modle = "Cutless Supreme", VIN = "A1" },
+                new Car {Make = "Nisan" , Modle = "Altima", VIN = "F6" }
+            };
+
+            Console.ReadLine();
+
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Modle { get; set; }
+            public string VIN { get; set; }
+        }
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string ISBN { get; set; }
+        }
+    }
+}
