76f9853e81cc758463defa4cfdd356b28ee01077
henry <henry49torres@gmail.com>
2025-01-19 19:15:41 -0400

    Iteracion For

--- FOR/For.cs
+++ FOR/For.cs
@@ -11,7 +11,7 @@ namespace For
         {
             for (int i = 0; i < 10; i++)
             {
-              //  Console.WriteLine(i);
+              Console.WriteLine(i);
               if(i == 7)
                 {
                     Console.WriteLine("Found seven!");
