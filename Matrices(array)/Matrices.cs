4b0ff7839562359f50a357009c166dfd3a61f404
henry <henry49torres@gmail.com>
2025-01-19 19:13:56 -0400

    update

--- Matrices(array)/Matrices.cs
+++ Matrices(array)/Matrices.cs
@@ -9,7 +9,7 @@ namespace Matrices
     {
         static void Main(string[] args)
         {
-            /*
+            
             int number1 = 4;
             int number2 = 8;
             int number3 = 15;
@@ -25,42 +25,41 @@ namespace Matrices
             else if (number3 == 16)
             {
             }
-            */
+            
 
-            /*
-            //array of 5 indeces
+           
             int[] numbers = new int[5];
             numbers[0] = 4;
             numbers[1] = 8;
             numbers[2] = 15;
             numbers[3] = 16;
             numbers[4] = 23;
-            //numbers[5] = 42;
+            numbers[5] = 42;
 
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers.Length);
             Console.ReadLine();
-            */
+            
 
 
-            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
-            //string[] names = new string[] { "eddie", "Alex", "Michael", "David Lee" };
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+            string[] names = new string[] { "eddie", "Alex", "Michael", "David Lee" };
 
-            /*
+            
             for (int i = 0; i < names.Length; i++)
             {
                 Console.WriteLine(names[i]);
             }
             Console.ReadLine();
-            */
+            
 
-            /*
+            
             foreach(string name in names)
             {
                 Console.WriteLine(name);
             }
             Console.ReadLine();
-            */
+            
 
             string zig = "You can get what you want out of life if you help enough other people get what they want.";
 
